15d6b60b52ee4662c13481ca460e31b91ba53f53
Alex Ross <alexander.w.rosss@gmail.com>
2020-04-16 21:38:10 -0400

    added screens and navigation

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import "react-native-gesture-handler";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import AllWorkoutsScreen from "./screens/AllWorkoutsScreen";
+import NewWorkoutScreen from "./screens/NewWorkoutScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Open up App.j to start working on your app!</Text>
-      </View>
+      <Tab.Navigator>
+        <Tab.Screen name="All Workouts" component={AllWorkoutsScreen} />
+        <Tab.Screen name="New Workout" component={NewWorkoutScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
